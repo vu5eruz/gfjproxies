ca3f88355d1e3c004f5b231cd541e612a67385a7
vu5eruz <undefined@disroot.org>
2026-02-16 14:00:23 -0400

    Run format and lint

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,9 @@ export default defineConfig(
 	...svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		linterOptions: {
+			reportUnusedDisableDirectives: 'off'
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
